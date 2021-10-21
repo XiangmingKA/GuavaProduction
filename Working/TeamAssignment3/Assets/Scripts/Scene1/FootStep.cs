f77574e7a74a1988f91149b19455beb8e3d62cab
XiangmingKa <715524834@qq.com>
2021-10-20 23:52:10 -0400

    Finished scene1

---
+++ Working/TeamAssignment3/Assets/Scripts/Scene1/FootStep.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootStep : MonoBehaviour
+{
+    public GameObject threeYearsOld;
+    public GameObject sixYearsOld;
+    public GameObject tenYearsOld;
+
+    public GameObject threeYearsOldMemory;
+    public GameObject sixYearsOldMemory;
+    public GameObject tenYearsOldMemory;
+
+    public GameObject[] footSteps;
+    [Range(1, 10)]
+    public int footstepsToNext = 4;
+
+    int currentInteractionIndex = 0;
+    int currentFootstepIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        threeYearsOld.SetActive(false);
+        sixYearsOld.SetActive(false);
+        tenYearsOld.SetActive(false);
+        threeYearsOldMemory.SetActive(false);
+        sixYearsOldMemory.SetActive(false);
+        tenYearsOldMemory.SetActive(false);
+        foreach (var i in footSteps)
+        {
+            i.SetActive(false);
+        }
+
+        SpriteAppear(threeYearsOld);
+        FlashBack(threeYearsOldMemory);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonDown()
+    {
+        currentInteractionIndex++;
+        if (currentInteractionIndex == 5)
+        {
+            SpriteAppear(sixYearsOld);
+            FlashBack(sixYearsOldMemory);
+        }
+        else if (currentInteractionIndex == 10)
+        {
+            SpriteAppear(tenYearsOld);
+            FlashBack(tenYearsOldMemory);
+        }
+        else
+        {
+            SpriteAppear(footSteps[currentFootstepIndex++]);
+        }
+        
+    }
+
+    float fadeInTime = 1.0f;
+    void SpriteAppear(GameObject obj)
+    {
+        obj.SetActive(true);
+        StartCoroutine(SpriteFadeIn(obj));
+    }
+
+    IEnumerator SpriteFadeIn(GameObject sprite)
+    {
+        SpriteRenderer renderer = sprite.GetComponent<SpriteRenderer>();
+        float timer = .0f;
+        while(timer < fadeInTime)
+        {
+            timer += Time.deltaTime;
+
+            Color newColor = renderer.color;
+            newColor.a = timer / fadeInTime;
+            renderer.color = newColor;
+
+            yield return null;
+        }
+    }
+
+    void FlashBack(GameObject sprite)
+    {
+        StartCoroutine(FadeInAndOut(sprite));
+    }
+
+    IEnumerator FadeInAndOut(GameObject sprite)
+    {
+        SpriteRenderer renderer = sprite.GetComponent<SpriteRenderer>();
+        float timer = .0f;
+
+        yield return new WaitForSeconds(fadeInTime);
+
+        sprite.SetActive(true);
+
+        while (timer < fadeInTime)
+        {
+            timer += Time.deltaTime;
+
+            Color newColor = renderer.color;
+            newColor.a = timer / fadeInTime;
+            renderer.color = newColor;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(fadeInTime);
+
+        timer = .0f;
+        while (timer < fadeInTime)
+        {
+            timer += Time.deltaTime;
+
+            Color newColor = renderer.color;
+            newColor.a =  1.0f - (timer / fadeInTime);
+            renderer.color = newColor;
+
+            yield return null;
+        }
+
+        sprite.SetActive(false);
+    }
+}
