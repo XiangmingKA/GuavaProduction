e6d1de52411820886913c7fdcbf7844332ae6e15
hsuanju-wang <77294600+hsuanju-wang@users.noreply.github.com>
2021-10-21 21:39:22 -0400

    Upload scene2_1

---
+++ Working/TeamAssignment3/Assets/Scripts/Scene2/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        StartCoroutine("ChangeScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(4f);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex+1);
+    }
+
+}
