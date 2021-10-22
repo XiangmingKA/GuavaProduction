42032898f56a4e404a3b9e58d50d9a3dff1a0021
hsuanju-wang <77294600+hsuanju-wang@users.noreply.github.com>
2021-10-22 02:08:08 -0400

    Update game scene effects

--- Working/TeamAssignment3/Assets/Scripts/Scene2/GameManager.cs
+++ Working/TeamAssignment3/Assets/Scripts/Scene2/GameManager.cs
@@ -5,24 +5,130 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance;
-    // Start is called before the first frame update
+    public GameObject scene2_1;
+    public GameObject scene2_2;
+    public GameObject scene2_3;
+    public GameObject scene2_4;
+    public GameObject scene2_5;
+    public GameObject[] drawingItems2_3;
+    public GameObject[] drawingItems2_4;
+    public GameObject[] drawingItems2_5;
+    public GameObject[] Items2_3;
+    public GameObject[] Items2_4;
+    public GameObject canvas;
+
+    [Range(1f, 10f)]
+    public float fadeInTime = 2.0f;
+    [Range(1f, 10f)]
+    public float stayTime = 3.0f;
+
     void Start()
     {
+        drawingItems2_3 = GameObject.FindGameObjectsWithTag("drawingItems2_3");
+        drawingItems2_4 = GameObject.FindGameObjectsWithTag("drawingItems2_4");
+        drawingItems2_5 = GameObject.FindGameObjectsWithTag("drawingItems2_5");
+        Items2_3 = GameObject.FindGameObjectsWithTag("2_3items");
+        Items2_4 = GameObject.FindGameObjectsWithTag("2_4items");
         StartCoroutine("ChangeScene");
     }
-
-    // Update is called once per frame
     void Update()
     {
         
     }
 
-    public IEnumerator ChangeScene()
+    IEnumerator ChangeScene()
     {
-        yield return new WaitForSeconds(4f);
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex+1);
+        yield return new WaitForSeconds(stayTime);
+        FlashBack(scene2_1);
+        yield return new WaitForSeconds(stayTime);
+        FlashBack(scene2_2);
+    }
+    IEnumerator ChangeScene2()
+    {
+        setClothesColor();
+        GroupFlashBack(drawingItems2_3, Items2_3);
+        canvas.SetActive(false);
+        yield return new WaitForSeconds(stayTime);
+        GroupFlashBack(drawingItems2_4, Items2_4);
+    }
+    public void ButtonClick()
+    {
+        StartCoroutine("ChangeScene2");
     }
 
+    void FlashBack(GameObject sprite)
+    {
+        StartCoroutine(FadeOut(sprite));
+    }
+
+    IEnumerator FadeOut(GameObject sprite)
+    {
+        SpriteRenderer renderer = sprite.GetComponent<SpriteRenderer>();
+        float timer = .0f;
+        //yield return new WaitForSeconds(stayTime);
+
+        timer = .0f;
+        while (timer < fadeInTime)
+        {
+            timer += Time.deltaTime;
+
+            Color newColor = renderer.color;
+            newColor.a = 1.0f - (timer / fadeInTime);
+            renderer.color = newColor;
+
+            yield return null;
+        }
+
+        sprite.SetActive(false);
+    }
+
+    void setClothesColor()
+    {
+        for (int i = 0; i< drawingItems2_3.Length; i++)
+        {
+            drawingItems2_4[i].GetComponent<SpriteRenderer>().color = drawingItems2_3[i].GetComponent<SpriteRenderer>().color;
+            drawingItems2_5[i].GetComponent<SpriteRenderer>().color = drawingItems2_3[i].GetComponent<SpriteRenderer>().color;
+        }
+    }
+
+    void GroupFlashBack(GameObject[] sprites, GameObject[] items)
+    {
+        StartCoroutine(GroupFadeOut(sprites,items));
+    }
+    IEnumerator GroupFadeOut(GameObject[] sprites, GameObject[] items)
+    {
+        float timer = .0f;
+        //yield return new WaitForSeconds(stayTime);
+
+        timer = .0f;
+        while (timer < fadeInTime)
+        {
+            timer += Time.deltaTime;
+            foreach (var sprite in sprites)
+            {
+                SpriteRenderer renderer = sprite.GetComponent<SpriteRenderer>();
+                Color newColor = renderer.color;
+                newColor.a = 1.0f - (timer / fadeInTime);
+                renderer.color = newColor;
+            }
+            foreach (var item in items)
+            {
+                SpriteRenderer renderer = item.GetComponent<SpriteRenderer>();
+                Color newColor = renderer.color;
+                newColor.a = 1.0f - (timer / fadeInTime);
+                renderer.color = newColor;
+            }
+
+
+            yield return null;
+        }
+        foreach (var sprite in sprites)
+        {
+            sprite.SetActive(false);
+        }
+        foreach (var item in items)
+        {
+            item.SetActive(false);
+        }
+    }
 }
