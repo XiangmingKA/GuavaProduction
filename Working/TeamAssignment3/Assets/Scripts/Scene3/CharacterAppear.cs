8467b35c355112b70d06c6b7c7554793941e1710
XiangmingKa <715524834@qq.com>
2021-10-27 17:20:16 -0400

    Add reject marks in scene3

--- Working/TeamAssignment3/Assets/Scripts/Scene3/CharacterAppear.cs
+++ Working/TeamAssignment3/Assets/Scripts/Scene3/CharacterAppear.cs
@@ -7,12 +7,18 @@ public class CharacterAppear : MonoBehaviour
     [Range(.0f, 10f)]
     public float fadeInTime = 2f;
     GameObject textObj;
+    public GameObject[] rejectMarkObjs;
 
     void Start()
     {
         textObj = transform.Find("text").gameObject;
         GetComponent<SpriteRenderer>().enabled = false;
         textObj.GetComponent<SpriteRenderer>().enabled = false;
+
+        foreach(var i in rejectMarkObjs)
+        {
+            i.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -29,6 +35,14 @@ public class CharacterAppear : MonoBehaviour
     public void TextFadeIn()
     {
         StartCoroutine(Appear(1.0f, textObj));
+    }
+
+    public void MarkFadeIn()
+    {
+        foreach (var i in rejectMarkObjs)
+        {
+            StartCoroutine(Appear(1.5f, i));
+        }
     }
 
     IEnumerator Appear(float waitTime, GameObject obj)
